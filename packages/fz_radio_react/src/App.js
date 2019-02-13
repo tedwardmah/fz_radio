0d25c864eb5d56685b34e0bba99fd861417d21b9
Ted Mahoney <ted.mahoney22@gmail.com>
2019-02-12 20:31:08 -0500

    remove proxy and make react app stable

--- packages/fz_radio_react/src/App.js
+++ packages/fz_radio_react/src/App.js
@@ -1,25 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  state = { users: [] }
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }))
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user=>
-            <div key={user.id}>{user.username}</div>
-        )}
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div className="App">
+      <h1>FZ Radio</h1>
+    </div>
+  );
 }
 
 export default App;
